47ad09c1f61cea36794712da7721cd505221ac0e
Tarik Bennett <tarik.bennett@gmail.com>
2016-06-25 15:19:00 -0700

    server modifications

--- app.js
+++ app.js
@@ -1,29 +1,41 @@
 var express = require('express');
+var swig = require('swig');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+// I'M NEW - define our routes
+var index = require('./routes/index');
+var api = require('./routes/api');
+var admin = require('./routes/admin');
+var posts = require('./routes/posts');
+var projects = require('./routes/projects');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// I'M NEW - declaring the port from environment (as in the case of Heroku) or doing app.set('port', (process.env.PORT || 8080));
 
-// uncomment after placing your favicon in /public
+// I'M NEW - view engine setup
+var swig = new swig.Swig();
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html');
+
+//uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+// I'M NEW - initialize our routes
+app.use('/', index);
+app.use('/api', api);
+app.use('/admin', admin);
+app.use('/posts', posts);
+app.use('/projects', projects);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -34,8 +46,9 @@ app.use(function(req, res, next) {
 
 // error handlers
 
-// development error handler
+// development error handlder
 // will print stacktrace
+
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
@@ -56,5 +69,8 @@ app.use(function(err, req, res, next) {
   });
 });
 
+app.listen(app.get('port'), function() {
+  console.log('Listening to', app.get('port'));
+});
 
 module.exports = app;
